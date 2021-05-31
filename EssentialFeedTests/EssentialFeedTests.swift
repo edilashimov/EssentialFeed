24766bdeddc1c569a701a4f0e29ee7d7a92bf321
Edil Ashimov <edil@floatleft.tv>
2021-05-31 11:19:36 -0400

    Refractor by moving the test logic to a separate type 'HTTPClientSpy'. For this, we've changed the HTTPClient class from singleton to global shared instance, where we no longer need the private init. In addition, we've also move the 'requestedUrl' property to the client that was subclassed, where it overrides the getFrom method from 'HTTPClient'. This was 'HTTPClient' can be used only when needed, otherwise other Clients we will subclassed and called by the lower level classes.

--- EssentialFeedTests/EssentialFeedTests.swift
+++ EssentialFeedTests/EssentialFeedTests.swift
@@ -10,30 +10,37 @@ import XCTest
 
 class RemoteFeedLoader {
     func load() {
-        HTTPClient.shared.requestURL = URL(string: "https://google.com")
+        HTTPClient.shared.get(from: URL(string: "https://google.com")!)
     }
 }
 
 class HTTPClient {
-    static let shared = HTTPClient()
-    
-    private init() {}
-    
+    static var shared = HTTPClient()
+    func get(from url: URL) {}
+}
+
+class HTTPClientSpy: HTTPClient {
     var requestURL: URL?
+
+    override func get(from url: URL) {
+        requestURL = url
+    }
 }
 
 class RemoteFeedLoaderTests : XCTestCase {
         
     func test_init_doesNotRequestDataFromURL() {
         let _ = RemoteFeedLoader()
-        let client = HTTPClient.shared
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         
         XCTAssertNil(client.requestURL)
     }
     
     func test_requestDataFromURL() {
         let sut = RemoteFeedLoader()
-        let client = HTTPClient.shared
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         
         sut.load()
         XCTAssertNotNil(client.requestURL)
