b7cdda7b7bfe9e7764c38bce6ac86d5e002a6fc4
Edil Ashimov <edil@floatleft.tv>
2021-05-31 10:44:29 -0400

    Setup Project With Unit Tests. The Target is set to Mac, for easy and decoupled feature development

---
+++ EssentialFeed/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  EssentialFeed
+//
+//  Created by Edil Ashimov on 5/31/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
