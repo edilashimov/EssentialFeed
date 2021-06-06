2350726d842bef4f182a4a29822c00dda7cf036d
Edil Ashimov <edil@floatleft.tv>
2021-06-05 21:11:54 -0400

    Deliver new added invalid data enum case error on Non 200 HTTPURLResponse

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -9,7 +9,7 @@ import Foundation
 
 //MARK: MAIN HTTPCLIENT PROTOCOL
 public protocol HTTPClient {
-    func get(from url: URL, completion: @escaping (Error) -> Void)
+    func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void)
 }
 
 //MARK: REMOTE FEED LOADER FEATURE
@@ -18,7 +18,8 @@ public final class RemoteFeedLoader {
     private let client: HTTPClient
     
     public enum Error: Swift.Error {
-            case connectivity
+        case connectivity
+        case invalidData
     }
     
     public init(url: URL, client: HTTPClient) {
@@ -27,7 +28,13 @@ public final class RemoteFeedLoader {
     }
     
     public func load(completion: @escaping (Error) -> Void) {
-        client.get(from: url) { error in completion(.connectivity) }
+        client.get(from: url) { error, response in
+            if response != nil {
+                completion(.invalidData)
+            } else {
+                completion(.connectivity)
+            }
+        }
     }
     
 }
