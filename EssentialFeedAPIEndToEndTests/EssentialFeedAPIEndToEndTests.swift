fc44a92ca45f1dfa419e9e23d198334dd40a3d0b
Edil Ashimov <edil@floatleft.tv>
2021-07-25 18:31:05 -0400

    Add memory leak tracking for components in integration

--- EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -9,9 +9,8 @@ import XCTest
 import EssentialFeed
 
 class EssentialFeedAPIEndToEndTests: XCTestCase {
-
+    
     func test_endToEndTestServiceGETFeedResult_matchesFixedTestAccountData() {
-       
         switch getFeedResult() {
         case let .success(items)?:
             XCTAssertEqual(items.count, 8, "Expected 8 items in the test account feed")
@@ -23,7 +22,6 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
             XCTAssertEqual(items[5], expectedItem(at: 5))
             XCTAssertEqual(items[6], expectedItem(at: 6))
             XCTAssertEqual(items[7], expectedItem(at: 7))
-
             
         case let .failure(error)?:
             XCTFail("Expected Success feed result, got \(error) instead")
@@ -34,11 +32,13 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
     
     // MARK: - Helpers
     
-    private func getFeedResult() -> LoadFeedResult? {
+    private func getFeedResult(file: StaticString = #file, line: UInt = #line) -> LoadFeedResult? {
         let testServerURL = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5c52cdd0b8a045df091d2fff/1548930512083/feed-case-study-test-api-feed.json")!
         let client = URLSessionHTTPClient()
         let loader = RemoteFeedLoader(url: testServerURL, client: client)
-        
+        trackForMemoryLeaks(client, file: file, line: line)
+        trackForMemoryLeaks(loader, file: file, line: line)
+
         let exp = expectation(description: "Wait for completion")
         var receivedResult: LoadFeedResult?
         loader.load { result in
@@ -60,13 +60,13 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
     
     private func id(at index: Int) -> UUID {
         return UUID(uuidString: ["73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6",
-                "BA298A85-6275-48D3-8315-9C8F7C1CD109",
-                "5A0D45B3-8E26-4385-8C5D-213E160A5E3C",
-                "FF0ECFE2-2879-403F-8DBE-A83B4010B340",
-                "DC97EF5E-2CC9-4905-A8AD-3C351C311001",
-                "557D87F1-25D3-4D77-82E9-364B2ED9CB30",
-                "A83284EF-C2DF-415D-AB73-2A9B8B04950B",
-                "F79BD7F8-063F-46E2-8147-A67635C3BB01"][index])!
+                                 "BA298A85-6275-48D3-8315-9C8F7C1CD109",
+                                 "5A0D45B3-8E26-4385-8C5D-213E160A5E3C",
+                                 "FF0ECFE2-2879-403F-8DBE-A83B4010B340",
+                                 "DC97EF5E-2CC9-4905-A8AD-3C351C311001",
+                                 "557D87F1-25D3-4D77-82E9-364B2ED9CB30",
+                                 "A83284EF-C2DF-415D-AB73-2A9B8B04950B",
+                                 "F79BD7F8-063F-46E2-8147-A67635C3BB01"][index])!
     }
     
     private func description(at index: Int) -> String? {
