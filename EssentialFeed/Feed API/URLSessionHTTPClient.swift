a2f333db86a87ae690d3f6e1940e5c2ca5a57496
Edil Ashimov <eddyashimov@gmail.com>
2021-11-29 16:38:01 -0500

    Refractroings

--- EssentialFeed/Feed API/URLSessionHTTPClient.swift
+++ EssentialFeed/Feed API/URLSessionHTTPClient.swift
@@ -18,14 +18,16 @@ public class URLSessionHTTPClient: HTTPClient {
     
     public func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
         session.dataTask(with: url) { data, response, error in
+            completion(Result {
             if let error = error {
-                completion(.failure(error))
+                throw error
             }
             else if let data = data, let response = response as? HTTPURLResponse  {
-                completion(.success((data, response)))
+                return (data, response)
             } else {
-                completion(.failure(UnexpectedValuesRepresentation()))
+                throw UnexpectedValuesRepresentation()
             }
+            })
         }.resume()
     }
     
