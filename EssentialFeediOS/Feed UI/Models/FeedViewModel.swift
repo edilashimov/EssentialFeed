f6b851e72cc185af05672b038612633440094a8c
Edil Ashimov <eddyashimov@gmail.com>
2022-01-03 17:52:07 -0500

    Remove mutable state from FeedViewModel. The FeedViewModel only needs to forward state changes, so state is only transient.

---
+++ EssentialFeediOS/Feed UI/Models/FeedViewModel.swift
@@ -0,0 +1,31 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Edil Ashimov on 1/3/22.
+//
+
+import Foundation
+import EssentialFeed
+
+final class FeedViewModel {
+    private let feedLoader: FeedLoader
+    typealias Observer<T> = (T) -> Void
+    
+    init(feedLoader: FeedLoader) {
+        self.feedLoader = feedLoader
+    }
+    
+    var onLoadingStateChange: Observer<Bool>?
+    var onFeedLoad: Observer<[FeedImage]>?
+    
+    func loadFeed() {
+        onLoadingStateChange?(true)
+        feedLoader.load { [weak self] result in
+            if let feed = try? result.get() {
+                self?.onFeedLoad?(feed)
+            }
+            self?.onLoadingStateChange?(false)
+        }
+    }
+}
--- EssentialFeediOSTests/EssentialFeediOSTests.swift
+++ EssentialFeediOSTests/EssentialFeediOSTests.swift
@@ -28,16 +28,16 @@ final class FeedViewControllerTests: XCTestCase {
     
 //    func test_loadingFeedIndicator_isVisibleWhileLoadingFeed() {
 //        let (sut, loader) = makeSUT()
-//
+//        
 //        sut.loadViewIfNeeded()
 //        XCTAssertTrue(sut.isShowingLoadingIndicator, "Expected loading indicator once view is loaded")
-//
+//        
 //        loader.completeFeedLoading(at: 0)
 //        XCTAssertFalse(sut.isShowingLoadingIndicator, "Expected no loading indicator once loading completes successfully")
-//
+//        
 //        sut.simulateUserInitiatedFeedReload()
 //        XCTAssertTrue(sut.isShowingLoadingIndicator, "Expected loading indicator once user initiates a reload")
-//
+//        
 //        loader.completeFeedLoadingWithError(at: 1)
 //        XCTAssertFalse(sut.isShowingLoadingIndicator, "Expected no loading indicator once user initiated loading completes with error")
 //    }
