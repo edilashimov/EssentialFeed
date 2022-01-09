996cb930ea243590988a74f5d62bf9882c39cedc
Edil Ashimov <eddyashimov@gmail.com>
2022-01-08 21:44:33 -0500

    Decouple FeedPresenter from FeedLoader with an adapter in the Composition layer

--- EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -12,8 +12,9 @@ public final class FeedUIComposer {
     private init() {}
     
     public static func feedComposedWith(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
-        let presenter = FeedPresenter(feedLoader: feedLoader)
-        let refreshController = FeedRefreshViewController(loadFeed: presenter.loadFeed)
+        let presenter = FeedPresenter()
+        let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader: feedLoader, presenter: presenter)
+        let refreshController = FeedRefreshViewController(loadFeed: presentationAdapter.loadFeed)
         let feedController = FeedViewController(refreshController: refreshController)
         presenter.loadingView = WeakRefVirtualProxy(refreshController)
         presenter.feedView = FeedViewAdapter(controller: feedController, imageLoader: imageLoader)
@@ -24,7 +25,7 @@ public final class FeedUIComposer {
         return { [weak controller] feed in
             controller?.tableModel = feed.map { model in
                 FeedImageCellController(viewModel:
-                     FeedImageViewModel(model: model, imageLoader: loader, imageTransformer: UIImage.init))
+                                            FeedImageViewModel(model: model, imageLoader: loader, imageTransformer: UIImage.init))
                 
             }
         }
@@ -33,32 +34,56 @@ public final class FeedUIComposer {
 }
 
 private final class WeakRefVirtualProxy<T: AnyObject> {
-     private weak var object: T?
+    private weak var object: T?
+    
+    init(_ object: T) {
+        self.object = object
+    }
+}
 
-     init(_ object: T) {
-         self.object = object
-     }
- }
-
- extension WeakRefVirtualProxy: FeedLoadingView where T: FeedLoadingView {
-     func display(_ viewModel: FeedLoadingViewModel) {
-          object?.display(viewModel)
-      }
- }
+extension WeakRefVirtualProxy: FeedLoadingView where T: FeedLoadingView {
+    func display(_ viewModel: FeedLoadingViewModel) {
+        object?.display(viewModel)
+    }
+}
 
 private final class FeedViewAdapter: FeedView {
-     private weak var controller: FeedViewController?
-     private let imageLoader: FeedImageDataLoader
+    private weak var controller: FeedViewController?
+    private let imageLoader: FeedImageDataLoader
+    
+    init(controller: FeedViewController, imageLoader: FeedImageDataLoader) {
+        self.controller = controller
+        self.imageLoader = imageLoader
+    }
+    
+    func display(_ viewModel: FeedViewModel) {
+        controller?.tableModel = viewModel.feed.map { model in
+            FeedImageCellController(viewModel:
+                                        FeedImageViewModel(model: model, imageLoader: imageLoader, imageTransformer: UIImage.init))
+        }
+    }
+}
 
-     init(controller: FeedViewController, imageLoader: FeedImageDataLoader) {
-         self.controller = controller
-         self.imageLoader = imageLoader
+private final class FeedLoaderPresentationAdapter {
+     private let feedLoader: FeedLoader
+     private let presenter: FeedPresenter
+
+     init(feedLoader: FeedLoader, presenter: FeedPresenter) {
+         self.feedLoader = feedLoader
+         self.presenter = presenter
      }
 
-    func display(_ viewModel: FeedViewModel) {
-         controller?.tableModel = viewModel.feed.map { model in
-             FeedImageCellController(viewModel:
-                 FeedImageViewModel(model: model, imageLoader: imageLoader, imageTransformer: UIImage.init))
+     func loadFeed() {
+         presenter.didStartLoadingFeed()
+
+         feedLoader.load { [weak self] result in
+             switch result {
+             case let .success(feed):
+                 self?.presenter.didFinishLoadingFeed(with: feed)
+
+             case let .failure(error):
+                 self?.presenter.didFinishLoadingFeed(with: error)
+             }
          }
      }
  }
