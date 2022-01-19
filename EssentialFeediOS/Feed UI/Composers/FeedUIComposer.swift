69a5d15c3e5d4c8d8e24025407159ade66fb7587
Edil Ashimov <eddyashimov@gmail.com>
2022-01-19 14:12:34 -0500

    Extract the FeedViewController creation and configuration into a factory method

--- EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -13,12 +13,11 @@ public final class FeedUIComposer {
     
     public static func feedComposedWith(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
         let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader: feedLoader)
-        
-        let bundle = Bundle(for: FeedViewController.self)
-        let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
-        let feedController = storyboard.instantiateInitialViewController() as! FeedViewController
-        feedController.delegate = presentationAdapter
-        feedController.title = FeedPresenter.title
+       
+        let feedController = FeedViewController.makeWith(
+             delegate: presentationAdapter,
+             title: FeedPresenter.title)
+
         presentationAdapter.presenter = FeedPresenter(
             feedView: FeedViewAdapter(controller: feedController, imageLoader: imageLoader),
             loadingView: WeakRefVirtualProxy(feedController))
@@ -35,6 +34,17 @@ private final class WeakRefVirtualProxy<T: AnyObject> {
         self.object = object
     }
 }
+
+private extension FeedViewController {
+     static func makeWith(delegate: FeedViewControllerDelegate, title: String) -> FeedViewController {
+         let bundle = Bundle(for: FeedViewController.self)
+         let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
+         let feedController = storyboard.instantiateInitialViewController() as! FeedViewController
+         feedController.delegate = delegate
+         feedController.title = title
+         return feedController
+     }
+ }
 
 extension WeakRefVirtualProxy: FeedLoadingView where T: FeedLoadingView {
     func display(_ viewModel: FeedLoadingViewModel) {
